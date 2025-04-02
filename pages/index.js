2f1bded61457b5d34cd3174ffd20b6f5e54b6052
Nikolas Raposo Martins Vieira <62063505+NikolasRaposo@users.noreply.github.com>
2025-04-02 00:35:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Tititititi! Sou eu! </h1>
+  return <h1>Tititititi! Sou eu!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
